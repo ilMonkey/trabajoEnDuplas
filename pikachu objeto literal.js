2f875ad7efc7571b8236628ec26087c7a4a7bcb6
Mario Guerra <maritopsi@gmail.com>
2020-03-13 15:57:36 -0300

    pikachu modified

--- pikachu objeto literal.js
+++ pikachu objeto literal.js
@@ -1,12 +1,21 @@
-// var pokemon = {
-//     nombre: "Pikachu",
-//     atacar: function () {
-//             console.log("Toma rayito");        
-//     }, 
-//     saludar: function () {
-//         console.log("Hola, soy " + this.nombre);
-//     },
-// }
+var pokemon = {
+    nombre: "Pikachu",
+    atacar: function () {
+            console.log("Toma rayito");        
+    }, 
+    saludar: function () {
+        console.log("Hola, soy " + this.nombre);
+    },
+}
 
+var pokemonEvolucion = {
+    nombre: "Raichu",
+    atacar: function () {
+            console.log("Toma rayito SUPER");        
+    }, 
+    saludar: function () {
+        console.log("Hola, soy " + this.nombre);
+    },
+}
 
-// pokemon.atacar();
+pokemon.atacar();
